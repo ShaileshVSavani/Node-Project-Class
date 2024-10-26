089af82c195a12a79ea2a82334a65d659ee84be3
ShaileshVSavani <svs.rnw23@gmail.com>
2024-10-27 01:32:37 +0530

    todo

--- PR-2-TODO/todo/index.js
+++ PR-2-TODO/todo/index.js
@@ -2,27 +2,27 @@ const express = require('express');
 const app = express();
 const port = 8090;
 
-// Middleware to parse send bodies
+
 app.use(express.json());
 
-// Initial Todos
+
 let todos = [
   { title: 'HTML', isCompleted: true, id: 1 },
   { title: 'JavaScript', isCompleted: true, id: 2 },
   { title: 'React', isCompleted: false, id: 3 }
 ];
 
-// 1. GET route to send a welcome message
+
 app.get('/', (req, res) => {
   res.send('Welcome to the TODO API');
 });
 
-// 2. GET route to fetch all todos
+
 app.get('/todos', (req, res) => {
   res.send(todos);
 });
 
-// 3. POST route to add a new todo
+
 app.post('/addtodo', (req, res) => {
   const { title, isCompleted } = req.body;
   if (!title || isCompleted === undefined) {
@@ -39,7 +39,7 @@ app.post('/addtodo', (req, res) => {
   res.send(newTodo);
 });
 
-// 4. PATCH route to update a todo
+
 app.patch('/update/:id', (req, res) => {
   const { id } = req.params;
   const { title, isCompleted } = req.body;
@@ -55,7 +55,7 @@ app.patch('/update/:id', (req, res) => {
   res.send(todo);
 });
 
-// 5. DELETE route to delete a todo
+
 app.delete('/delete/:id', (req, res) => {
   const { id } = req.params;
   const index = todos.findIndex(t => t.id === parseInt(id));
@@ -68,7 +68,7 @@ app.delete('/delete/:id', (req, res) => {
   res.send({ deletedTodo, todos });
 });
 
-// 6. GET route to fetch a single todo by id
+
 app.get('/todo/:id', (req, res) => {
   const { id } = req.params;
   const todo = todos.find(t => t.id === parseInt(id));
@@ -80,7 +80,6 @@ app.get('/todo/:id', (req, res) => {
   res.send(todo);
 });
 
-// 7. GET route to filter todos by their completion status
 app.get('/findbystatus', (req, res) => {
   const { isCompleted } = req.query;
   if (isCompleted === undefined) {
@@ -91,7 +90,6 @@ app.get('/findbystatus', (req, res) => {
   res.send(filteredTodos);
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
